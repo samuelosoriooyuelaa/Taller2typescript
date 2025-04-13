e31d6fcc4ec96f969e82e4eda82f80cae8eba26b
samuelosoriooyuelaa <sd.osorio2@uniandes.edu.co>
2025-04-13 12:03:02 -0500

    Add files via upload

---
+++ TallerType2/Serie.js
@@ -0,0 +1,11 @@
+export class Serie {
+    constructor(id, nombre, canal, temporadas, descripcion, linkPagina, imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.canal = canal;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.linkPagina = linkPagina;
+        this.imagen = imagen;
+    }
+}
---
+++ TallerType2/Serie.ts
@@ -0,0 +1,16 @@
+export class Serie{
+    constructor(
+        public id: number,
+        public nombre: string,
+        public canal: string,
+        public temporadas: number,
+        public descripcion: string,
+        public linkPagina: string,
+        public imagen: string
+
+
+    ){}
+
+    
+
+}
